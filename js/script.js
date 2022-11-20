bf2ff1d81e6f7e2841c1d6ca32e1531473d785ed
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-11-20 22:51:35 +0000

    Added functionality

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-02-JS/sw.js", {
+    scope: "/ICS2O-Unit5-02-JS/",
   })
 }
 
@@ -19,18 +19,18 @@ if (navigator.serviceWorker) {
  * This function generates a random number.
  */
 const randomNum = Math.floor(Math.random() * 10) + 1
-const negativeRandomNum = Math.floor(Math.random() * 10) + 1
+const negativeRandomNum = Math.floor(Math.random() * -10) + 1
 /**
  * This function displays a randomly generated positive or negative number.
  */
 function myButtonClicked() {
-  buttonOnChecked = document.getElementById("flash1").checked
-  alert(buttonOnChecked)
+ var buttonOnChecked = document.getElementById("flash1").checked
+  
   if (buttonOnChecked == true) {
     document.getElementById("radio-button-value").innerHTML =
-      "<p>Value is: On</p>"
+      randomNum
   } else {
     document.getElementById("radio-button-value").innerHTML =
-      "<p>Value is: Off</p>"
+      negativeRandomNum
   }
 }
