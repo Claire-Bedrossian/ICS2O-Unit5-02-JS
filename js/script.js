1605558394d1f289b5dd97d093075def14149974
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-11-20 17:48:33 +0000

    added radio buttons

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
@@ -16,8 +16,21 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function displays an alert.
+ * This function generates a random number.
+ */
+const randomNum = Math.floor(Math.random() * 10) + 1
+const negativeRandomNum = Math.floor(Math.random() * 10) + 1
+/**
+ * This function displays a randomly generated positive or negative number.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  buttonOnChecked = document.getElementById('flash1').checked;
+alert(buttonOnChecked);
+  if (buttonOnChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+    "<p>Value is: On</p>"
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+    "<p>Value is: Off</p>"
+  }
 }
